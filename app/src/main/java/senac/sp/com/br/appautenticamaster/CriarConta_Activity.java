bbcb3662354c026710fdf5dfc40b716fe0960eb8
Bruno <bruno.batista82@hotmail.com>
2020-04-09 04:44:15 -0300

    Initial commit

---
+++ app/src/main/java/senac/sp/com/br/appautenticamaster/CriarConta_Activity.java
@@ -0,0 +1,14 @@
+package senac.sp.com.br.appautenticamaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CriarConta_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.criar_conta_layout);
+    }
+}
---
+++ app/src/main/java/senac/sp/com/br/appautenticamaster/Inicio_Activity.java
@@ -0,0 +1,14 @@
+package senac.sp.com.br.appautenticamaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Inicio_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio_layout);
+    }
+}
